227c2e95dfc7c4b9bc5dc24bccb424c1598b05b6
Robert <bob.hindla@gmail.com>
2021-06-25 12:23:31 -0400

    Added more code

---
+++ candy.py
@@ -0,0 +1,33 @@
+candy = {
+        "maker":{
+            "Mars":[
+                "1:3Musketeers",
+                "2:$100,000 Bar",
+                "3:Snickers",
+
+            ],
+            "Hershey":[
+                "1:Kisses",
+                "2:Milk_Chocolate",
+
+            ],
+             "Lindt":[
+                "Chocolate_Shoe_Laces"
+
+            ],     
+            
+            "Cadbury":[
+                "1:Bunny"
+                 "2:Peep"
+
+            ],
+       },
+       
+        "sugars":[
+            "1:glucose",
+            "2:fructose",
+            "3:super-sweet",
+        ],
+        }
+        
+
