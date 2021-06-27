7db736fb0e82130c86984c04203f5bd1e35f300d
Robert <bob.hindla@gmail.com>
2021-06-27 14:39:32 -0400

    added analysis

--- candy.py
+++ candy.py
@@ -4,6 +4,11 @@ candy = {
                 "1:3Musketeers",
                 "2:$100,000 Bar",
                 "3:Snickers",
+# maker is a dictionary key
+# mars is a dictionary key
+# the list is the value but its confusing-looking
+# each value in the dictionary looks like a key-value
+# pair, but it isn't.
 
             ],
             "Hershey":[
@@ -23,6 +28,8 @@ candy = {
             ],
        },
        
+       #sugars is a dictionary key, but not a subkey of 'maker'
+       # it is just another key in 'candy,' like 'maker'
         "sugars":[
             "1:glucose",
             "2:fructose",
